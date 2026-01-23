833c6270508f35a424bce11b1490e579d0aba8f7
S2310053 <S2310053@bi.no>
2026-01-23 12:54:52 +0100

    starting code

---
+++ code.py
@@ -0,0 +1,30 @@
+##
+#  This program loads, transforms and processes the data needed
+##
+
+##
+#  This section imports libraries needed
+#
+import pandas as pd
+import numpy as np
+
+##
+#  This section uploads, transforms and cleans the salmon price time series data
+#  @dataset Fish Pool Index 4-6 kg Norwegian salmon price
+#  
+fileName = "Fish_Pool_Data.xls"
+
+xls        = pd.ExcelFile(fileName)
+sheetNames = np.flip(np.array(xls.sheet_names))
+datasetList   = []
+
+for i in range(0, len(sheetNames)):
+    data            = f"data{sheetNames[i]}"
+    globals()[data] = pd.read_excel(fileName, sheet_name = sheetNames[i], skiprows= 1)
+    datasetList.append(data)
+
+datasets = [globals()[name] for name in datasetList]
+dataAll  = pd.concat(datasets, ignore_index = True)
+
+
+print(dataAll.tail())
